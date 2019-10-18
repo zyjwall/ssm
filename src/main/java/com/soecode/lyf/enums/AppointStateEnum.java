b0555baa3e6481376fe178aaf32099d39734ddee
zyjwall <47664041+zyjwall@users.noreply.github.com>
2019-10-18 21:49:06 +0800

    Update AppointStateEnum.java

--- src/main/java/com/soecode/lyf/enums/AppointStateEnum.java
+++ src/main/java/com/soecode/lyf/enums/AppointStateEnum.java
@@ -4,7 +4,7 @@ package com.soecode.lyf.enums;
  * 使用枚举表述常量数据字典
  */
 public enum AppointStateEnum {
-
+// 不会枚举，回去看
 	SUCCESS(1, "预约成功"), NO_NUMBER(0, "库存不足"), REPEAT_APPOINT(-1, "重复预约"), INNER_ERROR(-2, "系统异常");
 
 	private int state;
